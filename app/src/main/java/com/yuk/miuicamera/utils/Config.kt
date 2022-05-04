8ef0f5a889b17939d1e7e4b877ac163665d61568
Meetingfate <71997026+Meetingfate@users.noreply.github.com>
2022-05-04 17:03:49 +0800

    Update Config.kt

--- app/src/main/java/com/yuk/miuicamera/utils/Config.kt
+++ app/src/main/java/com/yuk/miuicamera/utils/Config.kt
@@ -6,7 +6,7 @@ import com.yuk.miuicamera.BuildConfig
 object Config {
     val AndroidSDK: Int = Build.VERSION.SDK_INT
     const val modulePackage = BuildConfig.APPLICATION_ID
-    const val hostPackage = "com.android.camera"
-    const val SP_NAME = "MiuiCameraPerf"
-    const val TAG = "MiuiCamera"
+    const val hostPackage = "com.dragon.read"
+    const val SP_NAME = "番茄vip"
+    const val TAG = "番茄vip"
 }
